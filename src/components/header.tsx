ddb4f27982a77c3404f5de11abe4216402337219
craigashields <craig_a_shields@hotmail.co.uk>
2026-06-02 16:18:41 +0100

    fix(header): move social icons below logo instead of below title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,18 +11,18 @@ const Header: React.FC = () => {
         <Link
           href="/"
           aria-label={siteMetadata.headerTitle}
-          className="justify-self-center sm:justify-self-start sm:col-start-1 sm:row-span-2"
+          className="justify-self-center sm:justify-self-start sm:col-start-1 sm:row-start-1"
         >
           <AcpSVG />
         </Link>
 
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
+        <Link href="/" aria-label={siteMetadata.headerTitle} className="sm:col-start-2 sm:row-start-1">
           <div className="text-center sm:text-left text-lg sm:text-2xl md:text-3xl leading-tight break-words font-semibold font-heading">
             {siteMetadata.headerTitle}
           </div>
         </Link>
 
-        <div className="justify-self-center sm:justify-self-start sm:col-start-2 sm:row-start-2">
+        <div className="justify-self-center sm:justify-self-start sm:col-start-1 sm:row-start-2">
           <SocialLinks />
         </div>
       </div>
